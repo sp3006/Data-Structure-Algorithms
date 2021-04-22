c6968dac8b8ce19cc119a99f46d3fb3d5b08af37
SushantParicharak <sushant.paricharak@gmail.com>
2021-04-22 11:47:18 -0400

    Commit

--- Task1.py
+++ Task1.py
@@ -18,3 +18,21 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+"""
+The text data (text.csv) has the following columns: 
+sending telephone number (string), receiving telephone number (string), timestamp of text message (string).
+so first two columns are telephone numbers
+The call data (call.csv) has the following columns: 
+calling telephone number (string), receiving telephone number (string), start timestamp of telephone call (string), 
+duration of telephone call in seconds (string)
+first two columns are pone numbers
+
+"""
+
+get_length_for_calls = len(calls)
+get_length_for_texts = len(texts)
+
+merge_data_sets = set(zip(texts , calls))
+
+print(merge_data_sets[0][0])
